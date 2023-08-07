b5518d4889e2486cad50ee78b43805d97bf2b865
terrytosh <tmt103@txstate.edu>
2023-08-07 05:22:19 -0800

    Simple MainGUI() added.

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import os
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+class MainGUI(QMainWindow):
+    def __init__(self):
+        super(MainGUI, self).__init__()
+        ui_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mainwindow.ui")
+        uic.loadUi(ui_file, self)
+        self.show()
+
+def main():
+    app = QApplication([])
+    window = MainGUI()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
