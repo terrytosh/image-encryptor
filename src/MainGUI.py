c53b4df44bda8b1b6bc4fee558e330e604f0cef5
terrytosh <tmt103@txstate.edu>
2023-08-08 00:33:34 -0800

    Functionality for selecting output dir...

--- src/MainGUI.py
+++ src/MainGUI.py
@@ -15,10 +15,23 @@ class MainGUI(QMainWindow):
         # Quit app when 'Exit' QAction clicked in the toolbar
         self.exit_action.triggered.connect(qApp.quit)
 
-        # Connect 'Execute Action' button to handle_execute_action_button_cliked() function
+        # Connect 'Execute Action' button to handle_execute_action_button_cliked()
         self.execute_action_button.clicked.connect(self.handle_execute_action_button_clicked)
+
+        # Connect 'Select Output Directory' button to handle_select_output_directory_button_clicked()
+        self.select_output_directory_button.clicked.connect(self.handle_select_output_directory_button_clicked)
 
     def handle_execute_action_button_clicked(self):
         # Print current selections from QComboBox's
         print(self.action_selection_box.currentText())
         print(self.algorithm_selection_box.currentText())
+    
+    def handle_select_output_directory_button_clicked(self):
+        print("Handling output direction selection...")
+        options = QFileDialog.Options()
+        options |= QFileDialog.ShowDirsOnly | QFileDialog.DontUseNativeDialog
+        output_directory = QFileDialog.getExistingDirectory(self, "Select Output Directory", options=options)
+
+        if output_directory:
+            print("Selected Output Directory:", output_directory)
+            # Here, you can use the selected directory for saving your data
